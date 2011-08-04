5db1bfd94353fc02332e900236e9762c2318d196
Bruno Gola <brunogola@gmail.com>
2011-08-03 21:42:26 -0300

    last modified

--- andy/pagamento/admin.py
+++ andy/pagamento/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 
 from pagamento.models import Payment
 
-admin.site.register(Payment, list_display=('user', 'created_at', 'status', 'paid'), list_filter=('created_at', 'status', 'paid'), search_fields=('user__first_name', 'user__last_name', 'user__email'))
+admin.site.register(Payment, list_display=('user', 'created_at', 'status', 'paid', 'last_modified'), list_filter=('created_at', 'status', 'paid'), search_fields=('user__first_name', 'user__last_name', 'user__email'))
