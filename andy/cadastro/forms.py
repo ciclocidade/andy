f4b34d36ae20e66c3ff3221bdc1965f2f0f5ef7e
Bruno Gola <brunogola@gmail.com>
2011-08-03 22:03:28 -0300

    bugfix no forms

--- andy/cadastro/forms.py
+++ andy/cadastro/forms.py
@@ -36,7 +36,7 @@ class MemberForm(forms.ModelForm):
         self.cleaned_data = super(MemberForm, self).clean(*args, **kwargs)
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-                raise forms.ValidationError(_(u'You must type the same password each time'))
+                raise forms.ValidationError(u"Você deve digitar a senha duas vezes")
         return self.cleaned_data
 
     class Meta:
