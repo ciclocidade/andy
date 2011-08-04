c7095136f0db648d77fa0bbf2d1956ce6d685f20
Bruno Gola <brunogola@gmail.com>
2011-08-03 21:34:18 -0300

    adicionadno null e valores default

--- andy/pagamento/models.py
+++ andy/pagamento/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 class Payment(models.Model):
     created_at = models.DateTimeField(u"Data", auto_now_add=True)
-    paid = models.BooleanField(u"Pago")
-    user = models.ForeignKey(User)
-    status = models.CharField(u"Status", max_length=20, choices=(
+    paid = models.BooleanField(u"Pago", default=False)
+    user = models.ForeignKey(User, null=True)
+    status = models.CharField(u"Status", max_length=20, default="Aprovado", choices=(
                             ('Cancelado', 'Cancelado'),
                             ('Aguardando', 'Aguardando'),
                             ('Aprovado', 'Aprovadao')))
